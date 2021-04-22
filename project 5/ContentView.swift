7f9e0346cd01d878ad1083b2bf1c8734bc8bc9d3
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-04-22 11:18:34 +0545

    Initial Commit

---
+++ project 5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project 5
+//
+//  Created by Anisha Lamichhane on 4/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
