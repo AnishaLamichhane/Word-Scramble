a1728e1ab1fafca4bae1242b8ae412a512d134af
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-04-29 20:06:44 +0545

    ContentView.swift

--- project 5/ContentView.swift
+++ project 5/ContentView.swift
@@ -8,16 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            //we have found the file
-            
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // 
-                //
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewword)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                    .padding()
+                
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
             }
         }
-        return Text("helo")
+        .navigationBarTitle(rootWord)
+    }
+    
+    func addNewword() {
+//        lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+      
+//        exit if the remaining string is empty
+        guard answer.count > 0 else {
+            return
+        }
+        
+//        extra validation to come
+        usedWords.insert(answer, at: 0)
+        newWord = ""
     }
     
 }
